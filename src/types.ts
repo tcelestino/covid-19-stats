c40d1cf53fd1b98298144703ac0e23451452a2c9
Tiago Celestino <tcelestino@gmail.com>
2020-03-20 20:29:43 -0300

    refactor: remove and change to interface

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,13 @@
-export type Country = {
+export interface Country {
   id: number;
   name: string;
   value: string;
-};
+}
 
-export type CountriesProps = {
+export interface CountriesProps {
   onSelected: Function;
-};
+}
 
-export type DetailsProps = {
+export interface DetailsProps {
   countryCode: string;
-};
-
-export type CountryData = Array<{
-  info: {};
-  total_active_cases: string;
-  total_cases: string;
-  total_deaths: string;
-  total_new_cases_today: string;
-  total_new_deaths_today: string;
-  total_recovered: string;
-  total_serious_cases: string;
-  total_unresolved: string;
-}>;
+}
