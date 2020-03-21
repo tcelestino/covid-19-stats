93e720272134efbb411ae79f2dee630d3b1d3f1c
tcelestino <tcelestino@gmail.com>
2020-03-21 21:14:59 +0000

    Deploying to gh-pages from  - 8b49a6e2b9e75f1184b8d190e140f753fcb46662 🚀

---
+++ precache-manifest.d83b187b27c9d0c1ade89c8add770bb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e940b378d8d4b106b1008356aeca33ef",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "2aa0d824bda76c87beb7",
+    "url": "/covid-19-stats/static/js/2.72e897c9.chunk.js"
+  },
+  {
+    "revision": "aae0757f3c3679f699f5d39bc18f0dca",
+    "url": "/covid-19-stats/static/js/2.72e897c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a404e0a8e281b78956",
+    "url": "/covid-19-stats/static/js/main.45d3649c.chunk.js"
+  },
+  {
+    "revision": "f890c96d58dad85deff5",
+    "url": "/covid-19-stats/static/js/runtime-main.b4405b2f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-stats/precache-manifest.9d0becb2ca7e0a71643254ea259f6bb7.js"
+  "/covid-19-stats/precache-manifest.d83b187b27c9d0c1ade89c8add770bb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-stats/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
