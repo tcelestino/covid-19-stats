846825301a3b55f40ffc48880a8db374a3e26c33
Tiago Celestino <tcelestino@gmail.com>
2020-03-22 00:35:34 -0300

    Updates

---
+++ precache-manifest.355f5c43cfb140958a5a4a889b8ef9c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ca8847df31e1800c6c8b2973a8cef5",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "ea600f23cf486e2ede8e",
+    "url": "/covid-19-stats/static/js/2.56268f34.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19-stats/static/js/2.56268f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398b29b97acac44cee24",
+    "url": "/covid-19-stats/static/js/main.83bcdfcf.chunk.js"
+  },
+  {
+    "revision": "a97935a10b12fd7e2ebc",
+    "url": "/covid-19-stats/static/js/runtime-main.e812401a.js"
+  }
+]);
